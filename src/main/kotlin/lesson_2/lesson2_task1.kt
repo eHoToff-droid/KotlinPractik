7e9540e3733b76c557ff421f858a7b9b7a59e1d7
Yudjin <djoni44@mail.ru>
2025-11-30 21:19:56 +0300

    KP-2-1 avergeBall

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+
+    val studentBall1=3
+    val studentBall2=4
+    val studentBall3=3
+    val studentBall4=5
+
+    val avergeBall: Float = (studentBall1+studentBall2+studentBall3+studentBall4)/4f
+    println(avergeBall)
+
+}
