a38f127be5ab2d1e1973614b923cc8c7a930dec0
Yudjin <djoni44@mail.ru>
2025-12-03 13:50:39 +0300

    KS-2-4 corrected

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,16 +1,16 @@
 package lesson_2
 
-const val CRYSTAL_ORE=7
-const val IRON_ORE=11
+const val CRYSTAL_ORE = 7
+const val IRON_ORE = 11
 //const val BUFF =1.2
 
 fun main() {
 
-    val bonusCrystal =CRYSTAL_ORE*1.2 - CRYSTAL_ORE
+    val bonusCrystal = CRYSTAL_ORE * 1.2 - CRYSTAL_ORE
     val formatedBonusCrystal = bonusCrystal.toInt()
     println("Вы получили дополнительно кристаллической руды:  $formatedBonusCrystal ")
 
-    val bonusIron =IRON_ORE*1.2 - IRON_ORE
+    val bonusIron = IRON_ORE * 1.2 - IRON_ORE
     val formatedBonusIron = bonusIron.toInt()
     println("Вы получили дополнительно железной руды: $formatedBonusIron ")
 
