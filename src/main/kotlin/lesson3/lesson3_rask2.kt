e000d402fb3e03efd4c1f5214838a41f3f7f6888
Yudjin <djoni44@mail.ru>
2025-12-03 14:18:50 +0300

    KS-3-1 greeting

---
+++ src/main/kotlin/lesson3/lesson3_rask2.kt
@@ -0,0 +1,17 @@
+package lesson3
+
+fun main() {
+
+    val firstName = "Татьяна"
+    var seconName = "Андрееева"
+    val thirdName = "Сергеевна"
+    var age = 20
+
+    println("$seconName $firstName $thirdName Возраст:$age")
+
+    seconName = "Сидорова"
+    age = 22
+
+    println("$seconName $firstName $thirdName Возраст:$age")
+
+}
