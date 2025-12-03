847c1749f028d208e1325983fc148afb44ba0ffe
Yudjin <djoni44@mail.ru>
2025-12-03 15:43:07 +0300

    KS-2-2 no comment

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,27 +1,20 @@
 package lesson_2
 
 fun main() {
-    //основной персонал
     val mainStaff = 50
 
-    //зп основного работника
     val salaryMain = 30000
 
-    //стажеры
     val newStaff = 30
 
-    //зп стажера
     val salaryNew = 20000
 
-    //  расходы на основных работников
     val costMain: Int = mainStaff * salaryMain
     println(costMain)
 
-    //  расходы на всех работников
     val costTotal: Int = costMain + newStaff * salaryNew
     println(costTotal)
 
-    //средняя зп в фирме
     val salaryAverage: Int = costTotal / (mainStaff + newStaff)
     println(salaryAverage)
 
