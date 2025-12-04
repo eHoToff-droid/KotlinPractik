31271b18f61fa55b539af4f3bcc13deca09de5cf
Yudjin <djoni44@mail.ru>
2025-12-04 14:09:30 +0300

    KS-2-5 corrected

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -3,20 +3,16 @@ package lesson_2
 import kotlin.math.pow
 
 fun main() {
-
-    //Депозит
     val deposit = 70000
-    //Процентная ставка
     val interestRate: Double = 0.167
+    val numberYears = 20
+    val annual = 1
 
-    //считаем процент
-    val compoundInt = deposit * (1 + interestRate / 1).pow(20)
 
-    //Форматируем до 3х знаков после запятой и выводим
+    val compoundInt = deposit * (1 + interestRate / annual).pow(annual * numberYears)
+
     val formCompoundInt = (compoundInt * 1000).toInt()
     val formCompoundInt2: Double = formCompoundInt / 1000.toDouble()
     println(formCompoundInt2)
 
-    // Вариант форматирования ниже почему то выводит запятую а не точку
-//    val formCompoundInt = String.format("%.3f",compoundInt)
 }
