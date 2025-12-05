b3ab75504790345a0cd9d5fd28e521f4fad49ab2
Yudjin <djoni44@mail.ru>
2025-12-05 13:44:11 +0300

    KS-2-5 percetn changed

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,12 +4,12 @@ import kotlin.math.pow
 
 fun main() {
     val deposit = 70000
-    val interestRate: Double = 0.167
+    val interestRate: Double = 16.7
     val numberYears = 20
     val annual = 1
 
 
-    val compoundInt = deposit * (1 + interestRate / annual).pow(annual * numberYears)
+    val compoundInt = deposit * (1 + interestRate / 100 * annual).pow(annual * numberYears)
 
     val formCompoundInt = (compoundInt * 1000).toInt()
     val formCompoundInt2: Double = formCompoundInt / 1000.toDouble()
