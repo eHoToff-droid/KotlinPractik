63cad22e3812cc633419efba1f459b70cc337c87
Yudjin <djoni44@mail.ru>
2025-12-08 18:21:23 +0300

    KS-5-3 lottery

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -1,6 +1,7 @@
 package lesson5
 
 fun main() {
+
     val winNumber1 = 4
     val winNumber2 = 16
 
@@ -10,16 +11,21 @@ fun main() {
     println("Введите второе число от 0 до 42:")
     val number2 = readln().toInt()
 
-    var matchedCount = 0
+    val guessNumber: Int
 
-    if (number1 == winNumber1 || number1 == winNumber2) {
-        matchedCount++
-    }
-    if (number2 == winNumber1 || number2 == winNumber2) {
-        matchedCount++
+    if ((number1 == winNumber1 || number1 == winNumber2) &&
+        (number2 == winNumber1 || number2 == winNumber2)
+    ) {
+        guessNumber = 2
+    } else if (number1 != winNumber1 && number1 != winNumber2 &&
+        number2 != winNumber1 && number2 != winNumber2
+    ) {
+        guessNumber = 0
+    } else {
+        guessNumber = 1
     }
 
-    when (matchedCount) {
+    when (guessNumber) {
         0 -> println("Неудача! Выигрившие числа $winNumber1 и $winNumber2")
         1 -> println("Вы выиграли утешительный приз! Выигрившие числа $winNumber1 и $winNumber2")
         2 -> println("Поздравляем! Вы выиграли главный приз!")
