547265df000aca252e9b28f9b2609328e2dc4f0a
Yudjin <djoni44@mail.ru>
2025-12-08 17:59:08 +0300

    KS-4-5 corrected

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -2,19 +2,25 @@ package lesson_4
 
 fun main() {
 
-    val noDamage = false
+    val hasDamage = false
     val numberOfCrew = 69
     val boxFood = 60
     val weatherGood = true
 
-    val conditionForSalling: Boolean = ((noDamage == NO_DAMAGE) && (numberOfCrew in MIN_CREW..MAX_CREW)
-            && (boxFood > 50) && (weatherGood == WEATHER_GOOD)) || ((noDamage != NO_DAMAGE)
-            && (numberOfCrew == MAX_CREW) && (boxFood > 50) && (weatherGood == WEATHER_GOOD))
+    val conditionForSalling: Boolean = ((hasDamage == HAS_DAMAGE) &&
+            (numberOfCrew in MIN_CREW..MAX_CREW) &&
+            (boxFood > MIN_PROVISION) &&
+            (weatherGood == WEATHER_GOOD)) ||
+            ((hasDamage != HAS_DAMAGE) &&
+                    (numberOfCrew == MAX_CREW) &&
+                    (boxFood > MIN_PROVISION) &&
+                    (weatherGood == WEATHER_GOOD))
     println("Научно-исследовательский корабль может приступить к долгосрочному плаванию: $conditionForSalling")
 
 }
 
+const val MIN_PROVISION = 50
 const val MIN_CREW = 55
 const val MAX_CREW = 70
-const val NO_DAMAGE = true
+const val HAS_DAMAGE = true
 const val WEATHER_GOOD = true
