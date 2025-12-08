f55a6a2f863117d50a6b191848fc88813d0ae2cb
Yudjin <djoni44@mail.ru>
2025-12-08 17:48:14 +0300

    KS-4-4 add variables

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,17 +3,15 @@ package lesson_4
 fun main() {
 
     val dayOfTraining = 5
-
-    val handAndPress = 1
-    val legsAndBack = 0
+    val isEven = 0
 
     println(
         """
         
-        Упражнения для рук:    ${dayOfTraining % 2 == handAndPress}
-        Упражнения для ног:    ${dayOfTraining % 2 == legsAndBack} 
-        Упражнения для спины:  ${dayOfTraining % 2 == legsAndBack} 
-        Упражнения для пресса: ${dayOfTraining % 2 == handAndPress}       
+        Упражнения для рук:    ${dayOfTraining % 2 != isEven}
+        Упражнения для ног:    ${dayOfTraining % 2 == isEven} 
+        Упражнения для спины:  ${dayOfTraining % 2 == isEven} 
+        Упражнения для пресса: ${dayOfTraining % 2 != isEven}       
         
         
     """.trimIndent()
